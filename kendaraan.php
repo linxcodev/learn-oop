e7e1bf06f25ce3929fc3cde8cf1e9e860e068bd8
linxcodev <fajrul452@gmail.com>
2018-05-04 05:58:27 +0700

    partial script in different file for easy used

--- kendaraan.php
+++ kendaraan.php
@@ -72,25 +72,4 @@ class kendaraan {
     return $this->harga;
   }
 }
-
-// create object for call the class
-$kendaraan1 = new kendaraan();
-// and can create object without ()
-$kendaraan2 = new kendaraan;
-
-// set value properties use object
-$kendaraan1->harga = 100000000;
-$kendaraan1->tahunPembuatan = 2005;
-$kendaraan1->bahanBakar = 'Premium';
-// set value function use object
-$kendaraan1->setMerek('Miok');
-
-// run method in class kendaraan use object
-echo $kendaraan1->dapatSubsidi();
-
-// for call properties value
-// procedure 1
-// echo " Merek = $kendaraan1->merek dengan harga = $kendaraan1->harga";
-// procedure 2
-echo ' Merek ' . $kendaraan1->bacaMerek() . ' dengan harga ' . $kendaraan1->bacaHarga();
 ?>
