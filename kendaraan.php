45fdf7ab760d5f4a25e6b291fb9e81bba3c42f98
linxcodev <fajrul452@gmail.com>
2018-05-03 20:56:01 +0700

    Learn oop 1 - create properties - create method - create object - set properties and function - run method - run properties value with object->properties and function when before set properties

---
+++ kendaraan.php
@@ -0,0 +1,96 @@
+<?php
+// create class
+class kendaraan {
+  // properties on the class is a definition of the class
+  var $warna;             // variable in class called properties
+  var $harga;             // var is same with public
+  var $merek;
+  var $jumlah_roda;       // propertie with method write snake_case
+  var $bahanBakar;        // propertie with method write camelCase
+  var $tahunPembuatan;
+
+  // function in class for working this class and as working the properties
+  // function in class, call method
+  function statusHarga()
+  {
+    // $this->harga for access properties $harga on a class
+      if ($this->harga > 50000000) {
+        $status = 'mahal'; // $status is not properties, because this variable no description on class
+      } else {
+        $status = 'murah';
+      }
+
+      return $status;
+  }
+
+  function dapatSubsidi()
+  {
+    if ($this->tahunPembuatan <= 2005 && $this->bahanBakar == 'Premium') {
+      $subsidi = 'Ya';
+    } else {
+      $subsidi = 'Tidak';
+    }
+
+    return $subsidi;
+  }
+
+  function hargaSecond()
+  {
+    // initial
+    $thnPmb = $this->tahunPembuatan;
+    $harga = $this->harga;
+
+    if ($thnPmb > 2005) {
+      $hargaSecond = 20/100*$harga;
+    }
+
+    if ($thnPmb >= 2000 && $thnPmb <= 2005) {
+      $hargaSecond = 30/100*$harga;
+    }
+
+    if ($thnPmb < 2000) {
+      $hargaSecond = 40/100*$harga;
+    }
+
+    return $hargaSecond;
+  }
+
+  // use parameter
+  function setMerek($merek)
+  {
+    // initial
+    $this->merek = $merek;
+  }
+
+  function bacaMerek()
+  {
+    return $this->merek;
+  }
+
+  function bacaHarga()
+  {
+    return $this->harga;
+  }
+}
+
+// create object for call the class
+$kendaraan1 = new kendaraan();
+// and can create object without ()
+$kendaraan2 = new kendaraan;
+
+// set value properties use object
+$kendaraan1->harga = 100000000;
+$kendaraan1->tahunPembuatan = 2005;
+$kendaraan1->bahanBakar = 'Premium';
+// set value function use object
+$kendaraan1->setMerek('Miok');
+
+// run method in class kendaraan use object
+echo $kendaraan1->dapatSubsidi();
+
+// for call properties value
+// procedure 1
+// echo " Merek = $kendaraan1->merek dengan harga = $kendaraan1->harga";
+// procedure 2
+echo ' Merek ' . $kendaraan1->bacaMerek() . ' dengan harga ' . $kendaraan1->bacaHarga();
+?>
